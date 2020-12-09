f8a127dca1a0ffa9ee785295f4b5bfbe3a640842
DUSSART Luke <lukedussart@hotmail.fr>
2020-12-09 14:00:45 +0100

    affichage contact

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -1,24 +1,42 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\MessageType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Message;
 
 /**
-     * @Route("/message", name="message_")
-     */
+ * @Route("/message", name="message_")
+ */
 class MessageController extends AbstractController
 {
-    /**
-     * @Route("/message", name="message")
-     */
-    public function index(): Response
-    {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
-        ]);
+
+  /**
+   * @Route("/contact", name="contact")
+   */
+  public function formContact(Request $request)
+  {
+    $contact = new Message();
+    $form = $this->createForm(MessageType::class, $contact);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $this->addFlash('notification', "Votre message a bien été envoyé");
+      $contact = $form->getData();
+      $contact->setDateHeureContact(new \DateTime());
+      //$gestionContact->creerContact($contact);
+
+      //$gestionContact->envoiMailContact($contact);
+
+
+      //return $this->redirectToRoute("contact_success");
     }
-    
+    return $this->render('message/contact.html.twig', [
+        'form' => $form->createView(),
+    ]);
+  }
 }
