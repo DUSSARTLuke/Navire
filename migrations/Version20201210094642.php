4da94e8e2e9ab3399c2014ecabb1f88a39e75039
DUSSART Luke <lukedussart@hotmail.fr>
2020-12-16 13:42:08 +0100

    chgt migrations

--- migrations/Version20201210094642.php
+++ migrations/Version20201210094642.php
@@ -23,7 +23,7 @@ final class Version20201210094642 extends AbstractMigration
         $this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038EA83FAE4');
         $this->addSql('CREATE TABLE aisshiptype (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(100) NOT NULL, ais_ship_type INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE ais_ship_type');
-        $this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038EA83FAE4');
+        //$this->addSql('ALTER TABLE navire DROP FOREIGN KEY FK_EED1038EA83FAE4');
         $this->addSql('ALTER TABLE navire ADD CONSTRAINT FK_EED1038EA83FAE4 FOREIGN KEY (le_type_id) REFERENCES aisshiptype (id)');
     }
 
